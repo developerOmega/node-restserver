68b01e802342b24d5ee597e11483f2a39ecdc42d
developerOmega <developerOmega@developerOmega.com>
2020-04-08 19:49:23 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,23 @@
 //===========================
 // Puerto
 //===========================
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 4000;
+
+//===========================
+// Entorno
+//===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===========================
+// Base de datos
+//===========================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27018/cafe';
+}
+else{
+    urlDB = 'mongodb+srv://developer:5VL8b8lEOv8zpeid@cluster0-e9ykm.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 require('./config/config');
@@ -9,36 +11,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
- 
-app.get('/usuario', function (req, res) {
-  res.json('get Ususario');
-})
- 
-app.post('/usuario', function (req, res) {
-    let body =req.body;
+app.use(require('./routes/usuario'));
 
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es nesesario'
-        });
-    }
-    
-    res.json({
-        body
+
+mongoose.connect(process.env.URLDB, 
+    {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}, 
+    (err, res) => {
+        if(err){
+            throw new Error(err);
+        }
+        else {
+            console.log(`Base de datos ONLINE`);
+        }
     });
-});
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Ususario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando en puerto", process.env.PORT);
