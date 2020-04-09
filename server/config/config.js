501a5a8d6b9f80749e001942fe1b0ded16bb4662
developerOmega <developerOmega@developerOmega.com>
2020-04-08 20:17:09 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27018/cafe';
 }
 else{
-    urlDB = 'mongodb+srv://developer:5VL8b8lEOv8zpeid@cluster0-e9ykm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
